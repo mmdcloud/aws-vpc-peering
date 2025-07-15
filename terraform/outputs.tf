ac9caed947b7951f8a54daea7bd7f1fd33b19c75
mmdcloud <madmaxcloudpractice@gmail.com>
2025-07-16 00:05:37 +0530

    update: modified output variables

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,7 +1,15 @@
-output "instance1_ip" {
+output "instance1_public_ip" {
   value = module.instance1.public_ip
 }
 
-output "instance2_ip" {
+output "instance2_public_ip" {
   value = module.instance2.public_ip
+}
+
+output "instance1_private_ip" {
+  value = module.instance1.private_ip
+}
+
+output "instance2_private_ip" {
+  value = module.instance2.private_ip
 }
